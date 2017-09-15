4bc1ba133ab8382f8fbca3366425db54f311979c
dbabic89 <dariobabic89@gmail.com>
2017-09-15 14:18:28 +0200

    adding celebs list and detail

--- app/src/main/java/com/example/android/moovies/utils/Constants.java
+++ app/src/main/java/com/example/android/moovies/utils/Constants.java
@@ -1,7 +1,6 @@
 package com.example.android.moovies.utils;
 
 public class Constants {
-
     public static final String TMDB_BASE_URL = "http://api.themoviedb.org/3/";
     public static final String URL_POSTER = "https://image.tmdb.org/t/p/w342";
     public static final String URL_BACKDROP = "https://image.tmdb.org/t/p/w780";
